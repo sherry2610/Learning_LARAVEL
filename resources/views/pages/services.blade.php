5bd4b75f53eef93d4723f1e5e819a3fa8396f029
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-12-11 18:06:07 +0500

    initial setup

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+      <h3>SERVICES PAGE</h3>
+      <p>This is services page</p>
+    </body>
+</html>
